8365766071388f075e231d6af39b49a3e13876a3
Krogager <krogsith@gmail.com>
2024-01-06 18:01:11 +0300

    still developing bat ai

--- Scripts/Characters/Enemies/Enemy_Bat.gd
+++ Scripts/Characters/Enemies/Enemy_Bat.gd
@@ -1,12 +1,12 @@
 extends CharacterBody2D
 
 
-const SPEED = 30
+const SPEED = 30.0
 
 @export var player: CharacterBody2D
 enum State {
-	Attack,
-	Wander
+	Wander,
+	Attack
 }
 var current_state = State.Wander
 var rng = RandomNumberGenerator.new()
@@ -15,32 +15,27 @@ var hp = 2
 
 
 func _ready():
-	rng.randomize()
+	$Wander_time.wait_time = rng.randf_range(1, 3)
 
 
 func _physics_process(delta):
 	if !dead:
-		#draw_line(global_position, player.global_position, Color.RED, 8.0)
 		if hp <= 0:
 			death()
 		state_switch()
-		print(current_state)
+		#print(current_state)
 		match current_state:
 			State.Wander:
-				velocity.x = 0
-				velocity.y = 0
+				$See_timer.stop()
+				#$Wander_time.autostart = true
+				print($Wander_time.time_left)
 			State.Attack:
+				$Wander_time.stop()
 				var dir = Vector2(0, 0)
 				velocity = dir*SPEED
 		anim()
 		move_and_slide()
 
-
-func makepath():
-	$NavigationAgent2D.target_position = player.global_position
-
-func _on_timer_timeout():
-	makepath()
 
 func anim():
 	if velocity.x < 0:
@@ -57,11 +52,17 @@ func _on_area_2d_body_entered(body):
 func state_switch():
 	var player_distance = player.position - position
 	if player_distance.length() <= 60:
-		$RayCast2D.target_position = player.global_position
 		#print($RayCast2D.target_position)
-		if $RayCast2D.is_colliding() == false:
+		var space_state = get_world_2d().direct_space_state
+		var query = PhysicsRayQueryParameters2D.create(global_position, player.global_position)
+		query.exclude = [self]
+		var result = space_state.intersect_ray(query)
+		#print(result)
+		if result ['collider'] == player:
+			$See_timer.start()
 			current_state = State.Attack
-			$End_Timer.start()
+		else:
+			pass
 
 
 func death():
@@ -69,11 +70,20 @@ func death():
 	dead = true
 	$AnimatedSprite2D.queue_free()
 	$CollisionShape2D.queue_free()
-	$RayCast2D.queue_free()
 	$Area2D.queue_free()
-	$End_Timer.queue_free()
 	if $AnimatedSprite2D.flip_h == true:
 		$DeadBody.flip_h = true
 	else:
 		pass
 	$DeadBody.visible = true
+
+
+func _on_wander_time_timeout():
+	velocity.x = rng.randf_range(-20.0, 20.0)
+	velocity.y = rng.randf_range(-20.0, 20.0)
+	$Wander_time.wait_time = rng.randf_range(2, 5)
+
+
+func _on_see_timer_timeout():
+	$Wander_time.start(0.1)
+	current_state = State.Wander
