c6af22bcf610c2288a4c236a7d3d68b6ed414a13
Krogager <krogsith@gmail.com>
2024-01-28 20:19:25 +0300

    added spread for db shotgun

--- Scripts/Objects/bullet.gd
+++ Scripts/Objects/bullet.gd
@@ -3,10 +3,11 @@ extends Area2D
 
 var speed = 450
 var damage = 1
+var spread = Vector2(randf_range(-1, 1), randf_range(-1, 1))
 
 
 func _physics_process(delta):
-	position += transform.x * speed * delta
+	position += transform.x * speed * delta + spread
 
 
 func _on_body_entered(body):
